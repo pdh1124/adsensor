b257f7d0045aa2aeab04fcdad8cd5c869f906712
pdh1124 <pdw3231@naver.com>
2021-03-11 16:35:52 +0900

    베이스 구분

--- prac08/src/casting/Casting.java
+++ prac08/src/casting/Casting.java
@@ -4,15 +4,15 @@ public class Casting {
 
 	public static void main(String[] args) {
 		
-		int num = 10;
-		byte b;
+		int num = 0;
+		byte b = 10;
 		
-		b = (byte)num; //명시적 형변환, 캐스팅(casting)
+		b = (byte) num; //명시적 형변환, 캐스팅(casting)
 		System.out.println(b);
 		
 		num = b; //묵시적 형변환.
 		System.out.println(num);
-		//참조 자료형도 형변환이 가능하다. 뭐가 더 큰지?
+		// 참조 자료형도 형변환이 가능하다. 뭐가 더 큰지?
 		// 부모 자료형 변수에 = 자식 자료형 변수를 담으려면 (묵시적 형변환 가능)
 		// 자식 자료형 변수에 = 부모 자료형 변수를 담으려면 (명시적 형변환 가능(무조건이지 않음))
 		
