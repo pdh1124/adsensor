6e3513ba34c5fbbcdc5edbf4830aa4ffb9ab9bdf
pdh1124 <pdw3231@naver.com>
2021-03-13 08:09:39 +0900

    기본베이스

--- prac08/src/casting/TireMain.java
+++ prac08/src/casting/TireMain.java
@@ -1,34 +1,148 @@
 package casting;
 
+//타이어를 비유해서 상속을 받고 다형성을 이용하도록 클래스를 설계하세요.
+//자식 클래스 : 4계절, 스포츠, 스노우, 오프로드,
+//부모 변수에 담은후, 호출했을때, 각각의 특징을 표시하도록 구현하세요.
+
 class Tire {
+	public void Rolling() {
+		System.out.println("굴러간다.");
+	}
+
+	public void Color() {
+		System.out.println("회색");
+	}
+
+	public void Amount() {
+		int a = 4;
+		System.out.println("갯수는 " + a);
+	}
 
 	public void Speed() {
 		int speed = 100;
-		System.out.println("속도 : " + speed); //이것을 이미 부모에 입력을 해줬는데 
-		System.out.print("바뀌지");
-		System.out.print("않을");
-		System.out.print("코드들");
+		System.out.println("속도 : " + speed);
 	}
 }
 
 class FourSeason extends Tire {
+	public void Point() {
+		System.out.println("4계절 사용 가능");
+	}
 
 	@Override
 	public void Speed() {
 		int speed = 90;
-		System.out.println("속도 : " + speed); //이거를 굳이 다시 넣어주어야 하나요?
+		//System.out.println("속도 : " + speed);
 	}
 }
 
+class Sport extends Tire {
+	public void Point() {
+		System.out.println("속도가 빠르다.");
+	}
+
+	public void Money() {
+		System.out.println("비싸다.");
+	}
+
+	@Override
+	public void Speed() {
+		int speed = 150;
+		//System.out.println("속도 : " + speed);
+	}
+
+	@Override
+	public void Color() {
+		System.out.println("빨간색");
+	}
+}
+
+class Snow extends Tire {
+	public void Point() {
+		System.out.println("눈이 올때 사용한다.");
+	}
+	
+	public void Thickness() {
+		System.out.println("두껍다.");
+	}
+	
+	@Override
+	public void Speed() {
+		int speed = 70;
+		//System.out.println("속도 : " + speed);
+	}
+
+	@Override
+	public void Color() {
+		System.out.println("파란색");
+	}
+}
+
+class OffRoad extends Tire {
+	public void Point() {
+		System.out.println("산에 오를때 사용한다.");
+	}
+	
+	public void Thickness() {
+		System.out.println("완전 두껍다.");
+	}
+	
+	@Override
+	public void Speed() {
+		int speed = 40;
+		//System.out.println("속도 : " + speed);
+	}
+
+	@Override
+	public void Color() {
+		System.out.println("검은색");
+	}
+}
 
 public class TireMain {
 	public static void main(String[] args) {
+		Tire CarOne = new Tire();
+		CarOne.Rolling();
+		CarOne.Color();
+		CarOne.Amount();
+		CarOne.Speed();
 		
-		Tire c1 = new FourSeason();
-		c1.Speed();
+		System.out.println("-----------------");
+				
+		Tire CarTwo = new FourSeason();
+		//CarTwo.Point();
+		//CarTwo.money();
+		CarTwo.Speed();
+		CarTwo.Amount();
+		CarTwo.Color();
 		
-		FourSeason c2 = new FourSeason();
-		c1.Speed();
+		if (CarTwo instanceof FourSeason) {
+			FourSeason CarTwoSeason = (FourSeason) CarTwo;
+			CarTwoSeason.Point();
+		}
+		
+		System.out.println("-----------------");
+		
+		FourSeason CarThree = new FourSeason();
+		CarThree.Point();
+		CarThree.Speed();
+		
+		System.out.println("-----------------");
+		
+		Sport CarFour = new Sport();
+		CarFour.Point();
+		CarFour.Money();
+		CarFour.Amount();
+		CarFour.Color();
+		
+		System.out.println("-----------------");
+		
+		Tire CarFive = new OffRoad();
+		CarFive.Rolling();
+		CarFive.Color();
+		CarFive.Amount();
+		CarFive.Speed();
+		
 		
 	}
 
