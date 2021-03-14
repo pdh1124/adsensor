0e2760a1e26bf7b5514929deecaec4546f819705
pdh1124 <pdw3231@naver.com>
2021-03-14 21:53:30 +0900

    이차원배열

--- prac09/src/generic/S03이차원배열.java
+++ prac09/src/generic/S03이차원배열.java
@@ -1,24 +1,37 @@
 package generic;
 
+import java.util.Arrays;
+
 public class S03이차원배열 { //이차원 배열
 	public static void main(String[] args) {
 		
-		double score[][] = { { 3.3, 3.4 }, // 1학년 1, 2학기 평점
+		double[][] score = { { 3.3, 3.4 }, // 1학년 1, 2학기 평점
 				{ 3.5, 3.6 }, // 2학년 1, 2학기 평점
 				{ 3.7, 4.0 }, // 3학년 1, 2학기 평점
 				{ 4.1, 4.2 } }; // 4학년 1, 2학기 평점
 		
 		double sum = 0;
 		
+		//전체 평점의 합
 		for (int y = 0; y < score.length; y++) { // 각 학년별로 반복 (행열 중 행 부분)
 			for (int term = 0; term < score[y].length; term++) { // 각학년의 학기별로 반복 (행열 중 열 부분)
 				sum += score[y][term]; // 전체 평점 합
 			}
 		}
+		
 		int n = score.length; // 배열의 행 개수, 4
 		System.out.println(score.length);
 		int m = score[0].length; // 배열의 열 개수, 2
 		System.out.println(score[0].length);
 		System.out.println("4년 전체 평점 평균은 " + sum / (n * m));
+		
+		
+		//배열 출력
+		for(int i=0; i < score.length; i++) {
+			System.out.println(Arrays.toString(score[i]));
+		}
+		
+		
+		
 	}
 }
