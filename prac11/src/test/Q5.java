f6ab5e65f2802ec2420aec971d935a0ab43ae1e8
pdh1124 <pdw3231@naver.com>
2021-03-19 23:10:29 +0900

    Q5 테스트문

---
+++ prac11/src/test/Q5.java
@@ -0,0 +1,16 @@
+package test;
+
+import java.util.Scanner;
+
+public class Q5 {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		String a = scan.next();
+		
+		System.out.println(a + " 555");
+		
+		scan.close();
+	}
+
+}
